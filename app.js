127e50cfa9c6afd57aab5b9a0ff2ead7a2a7ace4
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-11 03:42:53 +0000

    test

---
+++ app.js
@@ -0,0 +1,27 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const formDollarToYen = function (valueInDollar) {
+    let valueInYen = valueInDollar * 146.26; 
+    return valueInYen; 
+}
+
+const formYenToPound = function (valueInYen) {
+    let valueInPound = valueInYen * 0.0064; 
+    return valueInPound;
+}
+
+
+const sum = (a,b) => {
+    return a + b
+}
+console.log(sum(7,3));
+
+
+module.exports = {sum, fromEuroToDollar, formDollarToYen, formYenToPound};
+
